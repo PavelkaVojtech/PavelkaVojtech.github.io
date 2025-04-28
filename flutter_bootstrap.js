14a53d14b32de18b5bc6a5ee705ad3e080a2740f
Vojtech Pavelka <vojtapavelk@gmail.com>
2025-04-28 06:00:53 +0200

    krajske kolo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3106675547"
+    serviceWorkerVersion: "1556608062"
   }
 });
